a6acce116a5bcbd88fcb897328e18b127743984e
unknown <b2212300@photon.chitose.ac.jp>
2022-11-02 11:58:07 +0900

    kadai14終了

--- prmn2022a/src/lecture01/kadai14.java
+++ prmn2022a/src/lecture01/kadai14.java
@@ -1,4 +1,19 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class kadai14 {
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+        int sum=0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+            if (i % 2 == 0) {
+                sum = sum + array[i];
+            }
+        }
+        System.out.println("配列番号が偶数の値の総和は"+sum);
+    }
 }
