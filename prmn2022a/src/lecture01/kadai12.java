033859f23ecbbf0a5559d6afe517525c380bfb06
unknown <b2212300@photon.chitose.ac.jp>
2022-11-02 11:34:51 +0900

    kadai12終了

---
+++ prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+
+        int studentNumber = 2212300;
+
+        System.out.println("B"+studentNumber+" Ryota Mikami");
+    }
+}
